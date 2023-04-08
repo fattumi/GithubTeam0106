bb57f2ed4bfffb6371f1692e9ca16795378bf9dd
fattumi <nevresafatma@gmail.com>
2023-04-08 19:56:19 +0300

    11.gun devam

--- src/ders11_stringManipulation/C07_Soru.java
+++ src/ders11_stringManipulation/C07_Soru.java
@@ -7,20 +7,30 @@ public class C07_Soru {
         // - ismi daha uzun ise, isim ve soyismi ilk harf buyuk, kalanlar kucuk seklinde yazdirin
         // - soyisim dah uzun ise ismi ilk harfi buyuk digerleri kucuk, soyismi buyuk harflerle yazdirin.
 
-        String isim="Mehmet";
-        String soyisim="Harun";
+        String isim="MEHMet";
+        String soyisim="HARunGİL";
+        String yeniIsimSoyisim;
 
         if (isim.length()>soyisim.length()){
             // isim ve soyisim ilk harf buyuk kalanlar kucuk seklinde yazdirin
 
-            String yeniIsimSoyisim= isim.substring(0, 1).toUpperCase() +
+            yeniIsimSoyisim= isim.substring(0, 1).toUpperCase() +
                                     isim.substring(1).toLowerCase() +
                                     " " +
                                     soyisim.substring(0,1).toUpperCase() +
                                     soyisim.substring(1).toLowerCase();
         }else {
-            // ismi ilk harf buyuk digerleri kucuk, soyiami buyuk harflerle yazdirin.
+            // ismi ilk harf buyuk digerleri kucuk, soyismi buyuk harflerle yazdirin.
+
+            yeniIsimSoyisim=isim.substring(0,1).toUpperCase()+
+                    isim.substring(1).toLowerCase()+
+                    " "+
+                    soyisim.toUpperCase();
+
+
 
         }
+
+        System.out.println(yeniIsimSoyisim);
     }
 }
