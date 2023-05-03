ba036a14ce0f9bf85349291f98c1195a8f67fd12
fattumi <nevresafatma@gmail.com>
2023-05-03 14:42:37 +0300

    16.gün dersi bitti

--- src/ders16_doWhileLoop/C03_SifreKontrolu.java
+++ src/ders16_doWhileLoop/C03_SifreKontrolu.java
@@ -24,17 +24,93 @@ public class C03_SifreKontrolu {
         boolean sifreDogruMu;
 
         do{
-            System.out.println("Lutfen vir sifre giriniz");
+            System.out.println("Lutfen bir sifre giriniz");
             sifre=scan.nextLine();
             // tum hatalarini soylemesini istiyorsak
             // bagimsiz if cumleleri ve bayrak kullanmak zorundayiz
             bayrak=0;
             int sonuc= kucukHarfKontroluYap(sifre);
+            // kucuk harf kontrolu yapan bir method olusturalim
+            // kucuk harf varsa 1, yoksa 0 dondursun
+            bayrak = bayrak + sonuc;
 
 
+            // sifre buyuk harf icermelidr
+
+            sonuc = buyukHarfKonroluYap(sifre);
+            // buyuk harf kontrolu yapan bir method olusturalim
+            // buyuk harf varsa 1, yoksa 0 dondursun
+            bayrak = bayrak + sonuc;
+
+            bayrak=bayrak+sonuc;
+
+
+           // - Sifre ozel karakter icermelidir
+            sonuc=ozelKarakterKontroluYap(sifre);
+            bayrak=bayrak+sonuc;
+
+           // - Sifre en az 8 karakter icermelidir.
+
+            if (sifre.length()<8){
+                System.out.println("sifre en az 8 karakter olmalidir");
+
+            }else {
+                bayrak++;
+            }
+
+
+
+            // bayrak 4 ise sifre tamam, degilse hata vardir.
+
+        }while (bayrak != 4);
+        System.out.println("sifreniz basari ile kaydedilmistir");
+
+
+
+    }
+
+    public static int ozelKarakterKontroluYap(String sifre) {
+        int bayrak=0;
+        String ozelKarakterler = "'+%+&%&/()(/&";
+        // sifre  ozel karakter icermelidir.
+        // bunun icin bir tum harfleri kontrol edecek bir for loop olusturmaliyim
+        // eger sifrenin karakterler,nden bir tanesini ozelKarakterler Sringi iceriyorsa
+        // bizim icin yeterli olur
+
+        for (int i = 0; i <sifre.length() ; i++){
+            if (ozelKarakterler.contains(sifre.substring(i,i+1))){
+                bayrak++;
+                break;
+            }
+        }
+        // bayrak==0 ise ozel karakter yoktur, yani bir hata var bunu yazdiralim
+        if (bayrak ==0){
+            System.out.println("sifre ozel karakter icermelidir");
+            return 0;
+        }else return 1;
+
+    }
+
+    public static int buyukHarfKonroluYap(String sifre) {
+        int bayrak=0;
+        // - Sifre kucuk harf icermelidir
+        // - bunun icin tum harfleri kontrol edecek bir for loop olusturulmalidir
+        // - 1 tane kucuk harf bulmam yeterli olacak
+
+        for (int i = 0; i <sifre.length() ; i++) {
+
+            if (sifre.charAt(i)>='A' && sifre.charAt(i)<='Z'){
+                bayrak++;
+                break;
+            }
 
         }
 
+        // bayrak==0 ise kucuk harf yoktur, yani hata vardir, bunu yazdiralim
+        if (bayrak==0){
+            System.out.println("Sifre buyuk harf icermelidir");
+            return 0;
+        }else  return 1;
 
 
     }
@@ -57,7 +133,7 @@ public class C03_SifreKontrolu {
         // bayrak==0 ise kucuk harf yoktur, yani hata vardir, bunu yazdiralim
         if (bayrak==0){
             System.out.println("Sifre kucuk harf icermelidir");
-            return=0;
+            return 0;
         }else  return 1;
 
     }
