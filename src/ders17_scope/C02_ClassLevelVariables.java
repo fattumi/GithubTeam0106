098755a8e56713e026a1542e5db3dbcbbadfef0f
fattumi <nevresafatma@gmail.com>
2023-05-05 23:57:47 +0300

    17.gün dersi devam ediyor

---
+++ src/ders17_scope/C02_ClassLevelVariables.java
@@ -0,0 +1,27 @@
+package ders17_scope;
+
+public class C02_ClassLevelVariables {
+
+
+
+    // class level variables'lar method'larin disinda olusturulur
+    // genel kullanim en ustte olusturulmalidir.
+
+    static String hastaneismi="Yildiz Hastanesi"
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void method(){
+
+
+    }
+
+    public void method2(){
+
+
+    }
+}
