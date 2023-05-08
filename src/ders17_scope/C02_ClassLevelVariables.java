debf8f16f2dc4f51575b582a671899b3bed0d12a
fattumi <nevresafatma@gmail.com>
2023-05-08 23:48:20 +0300

    17.gün dersi devam ediyor

--- src/ders17_scope/C02_ClassLevelVariables.java
+++ src/ders17_scope/C02_ClassLevelVariables.java
@@ -7,20 +7,49 @@ public class C02_ClassLevelVariables {
     // class level variables'lar method'larin disinda olusturulur
     // genel kullanim en ustte olusturulmalidir.
 
-    static String hastaneismi="Yildiz Hastanesi"
+    static String hastaneismi="Yildiz Hastanesi";
+    static int hasaSayisi=23454;
+    static String basHekimIsmi;
+    String persIsmi="isim girilmedi";
+    String persTlf="telefon girilmedi";
+    int persYas;
+
+
 
 
     public static void main(String[] args) {
+        System.out.println(hastaneismi); // Yildiz Hastanesi
+        System.out.println(basHekimIsmi); // null
+        // System.out.println(persIsmi); // satatic olmadigi icin static main method'dan kullanilamaz
+
+        /*
+        Class levele variabler'a deger atamasi yapilmamis olursa
+        Java bu variabler 'lara data turune gore
+        default tanimlanan degerleri atar
+        default degerler
+        sayisal varianle'la :0
+        boolean : false
+        char :''
+        Objeler (String dahil) : null
+         */
 
 
     }
 
     public static void method(){
+        System.out.println(hasaSayisi);
+        hasaSayisi++;
+        // System.out.println(persIsmi);
 
 
     }
 
     public void method2(){
+        System.out.println(hastaneismi); // Yildiz Hastanesi
+        hasaSayisi++;
+        System.out.println(persIsmi); // Isim girilmedi
+        System.out.println(persYas); // 0
+
 
 
     }
---
+++ src/ders17_scope/C03_ObjectVariables.java
@@ -0,0 +1,26 @@
+package ders17_scope;
+
+public class C03_ObjectVariables {
+
+    static String hastaneismi="Yildiz Hastanesi";
+    static int hasaSayisi=23454;
+    static String basHekimIsmi;
+    String persIsmi="isim girilmedi";
+    String persTlf="telefon girilmedi";
+    int persYas;
+
+    /*
+    class level variable'larin scope'u tum class'dir
+    satic kelimesi ile isaretlenen variable'lara
+    class icerisinde heryerden ulasilabildigi icin diger adi class variables'dir.
+
+    static kelimesi ile isaretlenmeyen variable'lara ise instance var,able denir.
+    instance variable'lar objelere bagli olduklarindan
+    bu variable'larin adi object variables'dir.
+
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
